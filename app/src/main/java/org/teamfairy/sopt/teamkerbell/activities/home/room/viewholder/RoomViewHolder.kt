bb8f564b07b021aca94539a668f26f2066ce16ae
lumiere <heesung6701@naver.com>
2018-05-10 12:30:13 +0900

    change sever ver3 to  ver4 (Group was divided into Group and CharRoom, Joined was divided into joinedGroup and joinedRoom) add make group page add make invite page add qrcode scanner and generator

---
+++ app/src/main/java/org/teamfairy/sopt/teamkerbell/activities/home/room/viewholder/RoomViewHolder.kt
@@ -0,0 +1,19 @@
+package org.teamfairy.sopt.teamkerbell.activities.home.room.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import org.teamfairy.sopt.teamkerbell.R
+
+/**
+ * Created by lumiere on 2018-01-10.
+ */
+class RoomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val tvName : TextView = itemView.findViewById(R.id.li_tv_room_name)
+    val tvContent : TextView = itemView.findViewById(R.id.li_tv_content)
+    val tvTime : TextView= itemView.findViewById(R.id.li_tv_time)
+    val ivProfile : ImageView = itemView.findViewById(R.id.li_iv_profile)
+    val tvCount : TextView=itemView.findViewById(R.id.li_tv_count)
+}
---
+++ app/src/main/java/org/teamfairy/sopt/teamkerbell/activities/items/vote/adapter/ResultByChoiceListAdapter.kt
@@ -0,0 +1,61 @@
+package org.teamfairy.sopt.teamkerbell.activities.items.vote.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.*
+import org.teamfairy.sopt.teamkerbell.R
+import org.teamfairy.sopt.teamkerbell._utils.NetworkUtils
+import org.teamfairy.sopt.teamkerbell.network.USGS_REQUEST_URL.JSON_PHOTO
+import org.teamfairy.sopt.teamkerbell.network.USGS_REQUEST_URL.JSON_U_IDX
+import org.teamfairy.sopt.teamkerbell.utils.IntentTag
+import org.teamfairy.sopt.teamkerbell.utils.IntentTag.Companion.INTENT_USER
+import org.teamfairy.sopt.teamkerbell.viewholder.ChoiceListViewHolder
+import org.teamfairy.sopt.teamkerbell.viewholder.UserViewHolder
+
+
+/**
+ * Created by lumiere on 2017-12-30.
+ */
+class ResultByChoiceListAdapter(var dataList: ArrayList<HashMap<String, String>>, var mContext: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (dataList[position]["type"]) {
+            "header" -> {
+                val h = holder as ChoiceListViewHolder
+                h.tvCount.text=dataList[position]["count"]
+                h.tvContent.text=dataList[position]["content"]
+            }
+            else -> {
+                val h = holder as UserViewHolder
+                h.tvName.text=dataList[position]["name"]
+                if (NetworkUtils.getBitmapList(dataList[position][JSON_PHOTO], holder.ivProfile, mContext,"$INTENT_USER/${dataList[position][JSON_U_IDX]}"))
+                    holder.ivProfile.setImageResource(R.drawable.icon_profile_default_png)
+
+            }
+        }
+
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        return when (dataList[viewType]["type"]) {
+            "header" -> {
+                val mainView: View = LayoutInflater.from(parent!!.context).inflate(R.layout.li_vote_choice, parent, false)
+                ChoiceListViewHolder(mainView)
+            }
+            else -> {
+                val mainView: View = LayoutInflater.from(parent!!.context).inflate(R.layout.li_user, parent, false)
+                UserViewHolder(mainView)
+            }
+        }
+
+    }
+
+
+    override fun getItemCount(): Int = dataList.size;
+
+}
